ea31fee179219f5a96f40d0c623b1f0afd568f7b
Riki Montgomery and Sara Spink <sara@>
2017-10-18 09:35:46 -0700

    use ajax promises

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -4,19 +4,11 @@ $(document).ready(function() {
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     $('#location').val("");
-    $.ajax({
-      url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=Imperial&appid=${apiKey}`,
-      type: 'GET',
-      data: {
-      format: 'json'
-      },
-      success: function(response) {
+    $.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=Imperial&appid=${apiKey}`).then(function(response) {
         $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-        $('.showTemp').text(`The temperature in farenheit is ${response.main.temp} degrees.`);
-      },
-      error: function() {
-        $('#errors').text("There was an error processing your request. Please try again.")
-      }
+        $('.showTemp').text(`The temperature in Fahrenheit is ${response.main.temp} degrees.`);
+      }).fail(function(error) {
+        $('.errors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
     });
   });
 });
